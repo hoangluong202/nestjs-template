97a3f48dfce669656c07a6d5a3d8d026e49c7e11
hoangluong202 <hl01012002@gmail.com>
2024-01-21 16:26:06 +0700

    fix name of key ssh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ SCRIPT="sudo docker-compose -f docker-compose.staging.yml up -d"
 
 # SSH to server
 echo "SSH to server..."
-ssh -i ~/.ssh/key-pair-login.pem ubuntu@3.95.11.114 << EOF
+ssh -i ~/.ssh/id_rsa.pem ubuntu@3.95.11.114 << EOF
 
 # Navigate to project folder
 echo "Navigate to project folder..."
