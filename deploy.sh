1aff2ab2a6f1f40d9998ab942d645c0401c6dfef
hoangluong202 <hl01012002@gmail.com>
2024-01-21 16:23:23 +0700

    set up ssh in file ci cd

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,6 @@
 SERVER="ubuntu@3.95.11.114"
 REPO_DIR="nestjs-template"
 SCRIPT="sudo docker-compose -f docker-compose.staging.yml up -d"
-SSH_KEY_DIR="~/.ssh/"
-
-# Setup SSH
-echo "Setting up SSH..."
-if [ ! -d $SSH_KEY_DIR ]; then
-  mkdir -p $SSH_KEY_DIR
-fi
-echo "${{ secrets.SSH_PEM_KEY }}" > ~/.ssh/key-pair-login.pem
-chmod +x ~/.ssh/key-pair-login.pem
-ssh-keyscan 3.95.11.114 >> ~/.ssh/known_hosts
 
 # SSH to server
 echo "SSH to server..."
