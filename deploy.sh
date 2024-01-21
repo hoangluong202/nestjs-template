b571c6669cca2a077e41d647cb0a9be70089715a
hoangluong202 <hl01012002@gmail.com>
2024-01-21 16:52:51 +0700

    only deploy in ci cd file

--- deploy.sh
+++ deploy.sh
@@ -17,10 +17,7 @@ echo "Pull latest changes..."
 git pull origin main
 
 # Run docker-compose
-echo "Pull latest Docker images..."
-sudo docker compose -f docker-compose.staging.yml pull
 echo "Run docker-compose..."
-sudo docker compose -f docker-compose.staging.yml down
 sudo docker compose -f docker-compose.staging.yml up -d
 
 EOF
